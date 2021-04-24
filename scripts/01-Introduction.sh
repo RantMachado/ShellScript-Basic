3cbe8b8899e37840fd18241d0ee52381207def4f
RantMachado <talktorandolph@live.com>
2021-04-24 11:02:49 -0300

    [Feat/Variaveis] Declaracao de variaveis, var globais e locais e var de ambiente.

---
+++ scripts/01-Introduction.sh
@@ -0,0 +1,29 @@
+# Variáveis globais 
+
+echo $PATH # Lista de diretorio de programas executáveis
+echo $USERNAME # Nome do user logado
+echo $TERM # Tipo do terminal ou janela em uso
+echo $HOME # Diretório home do user atual
+echo $UID # UID do user atual
+echo $RANDOM # Gera um número aleatório 
+echo $LANG # Idioma do locale 
+
+
+# O comando env e printenv mostram as variáveis de ambiente no terminal
+
+# ---------------------------------------------------------------------
+
+# Declaração e atribuição de varíaveis 
+
+teste = valor # Se tiver espaço a variável não recebe a atribuição
+teste=valor2 # Atribuição correta
+
+echo $teste
+
+# Deixando a variavel como global, basta usar o comando export 
+
+export teste
+
+# Para remoção da variável de ambiente, basta usar o comando unset
+
+# unset teste
