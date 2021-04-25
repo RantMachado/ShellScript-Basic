c910e878c2749151b1ee70fca5745f3975caa66d
RantMachado <talktorandolph@live.com>
2021-04-25 13:05:45 -0300

    [Feat/SimpleScript] Criado o primeiro script e concedendo permissao de execuçao a ele.

---
+++ scripts/05-SimpleScript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Script simples, todo script criado precisa da permissão de execução, a permissão é concedida com o comando chmod a+x
+
+cd / 
+ls -l
+echo "Arquivos e diretorios listados"
+echo -n  "O usuário logado no momento é: " # o parametro -n retira a quebra de linha do comando echo
+whoami
