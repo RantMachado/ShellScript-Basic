2fc352ba088fec66faac3a9ef395122c622c0011
RantMachado <talktorandolph@live.com>
2021-05-02 19:54:23 -0300

    [Feat/Redirects] Entendendo o funcionamento dos redirects.

---
+++ scripts/08-RedirectCommOutput.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#exemplo do uso da crase
+
+#dia=`date`
+#echo "A data é $dia"
+
+
+#Copiar a listagem de um dir para arquivos únicos, de acordo com a data e hora.
+
+arq=`date +%d%m%y%H%M`
+
+ls -la /home/rant > log.$arq
+
+
+
+
+
---
+++ scripts/09-RedirectIO.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Redirecionamento de saida 
+# O mais comum é enviar a saída de um comando para um
+# arquivo. Para isso use o simbolo: > 
+# comando > arquivo_saida
+
+# ls > arq.txt 
+
+# Dessa forma o conteudo da saida vai para o arquivo criado e caso seja 
+# executado novamente ele sobrescreve o arquivo criado 
+
+
+# Caso queira acrescentar a saída e um comando a um arquivo em vez de 
+# sobrescrevê-lo, use o simbolo duplo: >>
+# comando >> arquivo_saida
+
+# ls >> arq.txt
+
+# Dessa forma o conteudo também é inserido no arquivo, mas caso rodado 
+# novamente o conteudo é anexado no fim do arquivo e não sobrescrito
+
+ls > arq.txt
+
+# Acrescentando data e hora
+date >> date.txt
+
+# Redirecionamento de entrada
+# Em vez de redirecionar a saída de um comando para um arquivo, é possível tomar 
+# o conteúdo de um arquivo e redireciona-lo para um comando.
+# Para isso, use o símbolo: <
+# comando > arquivo_entrada
+wc < arq.txt
+
+# O comando wc produz contagem de texto do arquivo. Por padrão mostra três 
+# valores:
+
+# - Números de linhas no texto
+# - Números de palavras
+# - Números de bytes
+
+# Redirecionamento de entrada inline
+# Com este método é possível especificar os dados para redirecionar na 
+# linha de comando em vez de um arquivo.
+# O simbolo de redirecionamento de entrada inline é o <<; Você também deve 
+# Especificar um marcador de texto que delineie o inicio e o fim dos dados 
+# usados para entrada: 
+
+# wc << EOF O marcador pode ser qualquer palavra no exemplo usado foi EOF
+# EOF Ao digitar o marcador e pressionar o enter o wc executa tudo que foi
+# digitado no terminal, graças a variavel ps2 do prompt secundário.
